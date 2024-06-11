b70d7bbb0a71bfbcb13f654afc4af32e6aa6f4d0
Ebenezer Oluwafemi <ebenco7@gmail.com>
2024-06-11 23:20:48 +0100

    The other pages with back button but Slider issue

--- app/breed/[breed]/page.tsx
+++ app/breed/[breed]/page.tsx
@@ -1,10 +1,28 @@
 "use client"
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function page() {
-
+    //The code below is a hook it can not be used anywhere else but under a function
     const Params = useParams()
+   
+    // async permits the server to continue to load the data, it does nit block it
+
+    const GetData = async()=>{
+
+        const Data = await fetch("https://dog.ceo/api/breed/hound/images").then((data)=>data.json()).then((data)=>{
+            console.log(data)
+        })
+        // Because this is a process will must tell it to wait
+
+
+
+    }
+
+    //The code below runs once at the first load, if loaded for the second time it wont run, a trigger is created to tell it to run when a certain value change.
+    useEffect(()=>{
+        alert("")
+    })
   return (
     <div>Inside breed folder  (Params.breed)</div>
   )
