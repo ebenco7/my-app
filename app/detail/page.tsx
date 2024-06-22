c366446723a0d820e7bdc303e3fe83e54659dc74
Ebenezer Oluwafemi <ebenco7@gmail.com>
2024-06-22 22:29:23 +0100

    Suspense wrapped around DetailPage

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -1,48 +1,49 @@
 "use client";
 
-import React, { Suspense, useState } from "react";
+import React, { Suspense } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
-
 import Image from "next/image";
 import AppButton from "../APP/COMPONENTS/AppButton";
 
-function Page() {
+function DetailPage() {
   const Qparams = useSearchParams();
   const router = useRouter();
   const Imageurl = Qparams.get("Imageurl");
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="h-screen w-screen bg-AppTertiary CENTER">
-      <div
-        className={cn(
-          "pt-12 w-[180px] CENTER h-screen",
-          "justify-start gap-[120px] flex-col"
-        )}>
-          <div onClick={() => router.back()}>
-            <AppButton>Back</AppButton>
-          </div>
-        </div>
-
-        <div className="w-full h-full pt-12 pl-12 p-12">
-          <h1 className="text-3xl font-bold capitalize text-AppMute">Image Url</h1>
-          <p>{Imageurl}</p>
-          {Imageurl ? (
-            <div className="h-[400px] w-[400px] relative mt-20">
-              <Image
-                src={Imageurl}
-                alt="Image"
-                fill // Ensures the image fills the container
-                style={{ objectFit: "cover" }} // Ensures the image covers the entire container
-                className="rounded-md border-2 border-AppPrimary"
-              />
-            </div>
-          ) : (
-            <p>No Query Found</p>
-          )}
+    <div className="h-screen w-screen bg-AppTertiary flex justify-center items-center">
+      <div className={cn("pt-12 w-[180px] flex flex-col justify-start gap-[120px] items-center")}>
+        <div onClick={() => router.back()}>
+          <AppButton>Back</AppButton>
         </div>
       </div>
+
+      <div className="w-full h-full pt-12 pl-12 p-12">
+        <h1 className="text-3xl font-bold capitalize text-AppMute">Image Url</h1>
+        <p>{Imageurl}</p>
+        {Imageurl ? (
+          <div className="h-[400px] w-[400px] relative mt-20">
+            <Image
+              src={Imageurl}
+              alt="Image"
+              fill
+              style={{ objectFit: "cover" }}
+              className="rounded-md border-2 border-AppPrimary"
+            />
+          </div>
+        ) : (
+          <p>No Query Found</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DetailPage />
     </Suspense>
   );
 }
